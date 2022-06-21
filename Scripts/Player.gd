c2efce6fa19a7c660a1a26bdc63fe737463a57a0
Kevin <21161498+spock166@users.noreply.github.com>
2022-06-20 22:50:34 -0500

    Base Commit

---
+++ Scripts/Player.gd
@@ -0,0 +1,41 @@
+extends KinematicBody2D
+
+# stats
+var health : int = 69
+# physics
+var speed : int = 200
+var jumpForce : int = 600
+var gravity : int = 800
+var vel : Vector2 = Vector2()
+var grounded : bool = false
+# components
+onready var sprite = $Sprite
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _physics_process(delta):
+	vel.x = 0
+	if Input.is_action_pressed("move_left"):
+		vel.x -= speed
+	if Input.is_action_pressed("move_right"):
+		vel.x += speed
+	
+	vel = move_and_slide(vel, Vector2.UP)
+	
+	vel.y += gravity * delta
+	
+	if Input.is_action_pressed("jump") and is_on_floor():
+		vel.y -= jumpForce
+		
+	if vel.x < 0:
+		sprite.flip_h = true
+	elif vel.x > 0:
+		sprite.flip_h = false
