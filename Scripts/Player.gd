541ba74ddc49d75ff03432edec5cfd1803eabc29
spock166 <21161498+spock166@users.noreply.github.com>
2022-06-25 19:22:05 -0500

    Updated Jump
    -No longer have the ability to change direction mid-air.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -22,10 +22,13 @@ func _ready():
 
 
 func _physics_process(delta):
-	vel.x = 0
-	if Input.is_action_pressed("move_left"):
+	
+	if is_on_floor():
+		 vel.x = 0
+	
+	if Input.is_action_pressed("move_left") and is_on_floor():
 		vel.x -= speed
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("move_right") and is_on_floor():
 		vel.x += speed
 	
 	vel = move_and_slide(vel, Vector2.UP)
